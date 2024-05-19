6da7dae630201b7efef9edd7b9739036a5a766c9
Bryan Reed <bryan87reed@gmail.com>
2024-05-19 11:13:04 -0700

    mailing works

--- src/index.js
+++ src/index.js
@@ -1,20 +1,57 @@
+require('dotenv').config();
+
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+const mailgun = require('mailgun-js');
+const API_KEY = process.env.API_KEY;
+const DOMAIN = process.env.DOMAIN;
 
 app.use(cors());
+app.use(express.json());
 
-
-function delay(ms) {
+const delay = (ms) => {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const sendMail = (email, filePath) => {
+  const mg = mailgun({ apiKey: API_KEY, domain: DOMAIN });
+
+  const data = {
+    from: 'Excited User <bronsky@sandbox91a3c218fa1a4fe6a5c44e86c626fb59.mailgun.org>',
+    to: email,
+    subject: 'Hello',
+    text: 'Testing some Mailgun awesomeness!',
+    attachment: path.join(__dirname, filePath)
+  };
+
+  // Send the email
+  mg.messages().send(data, function (error, body) {
+    if (error) {
+      console.log('Error:', error);
+    } else {
+      console.log('Email sent:', body);
+    }
+  });
+}
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.post('/sendmap', (req, res) => {
+  const email = req?.body?.email;
+  console.log('email', email)
+  if (email) {
+    sendMail(email, 'map.jpeg');
+    res.status(200).send({ message: "Email sent successfully!" });
+  } else {
+    res.status(400).send({ error: "Email is required" });
+  }
+})
 
 app.get("/download", async (req, res) => {
   const filePath = path.join(__dirname, "map.jpeg");
